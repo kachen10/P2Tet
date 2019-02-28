e4f7ac484afdb749e327151d226c79107a8eb8c6
KChen <karencyuan@gmail.com>
2019-02-27 19:49:37 -0800

    fixing

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const PORT = process.env.PORT || 5000
 
 express()
     .use(express.static(path.join(__dirname, 'public')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('pages/index'))
+    .get('/', (req, res) => res.render('client/index'))
     .listen(PORT, () => console.log(`Listening on ${PORT}`))
-app.use(express.static('public/client'))
+
 var io = require('socket.io')(server);
