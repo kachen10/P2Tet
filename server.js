09c81c8dd598bc2dfb2e3c8d80e0feed893ad447
Yuan Chen <karencyuan@gmail.com>
2019-02-28 00:00:51 -0800

    updating server

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 
 const express = require('express')
-const path = require('path')
 var app = express();
 var server = app.listen(process.env.PORT || 5000, listen);
 
@@ -11,3 +10,55 @@ function listen() {
 }
 
 app.use(express.static('public/client/'));
+
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
+
+setInterval(heartbeat, 33);
+
+function heartbeat() {
+    io.sockets.emit('heartbeat', blobs);
+}
+
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+    // We are given a websocket object in our function
+    function (socket) {
+
+        console.log("We have a new client: " + socket.id);
+
+
+        socket.on('start',
+            function (data) {
+                console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
+                var blob = new Blob(socket.id, data.x, data.y, data.r);
+                blobs.push(blob);
+            }
+        );
+
+        socket.on('update',
+            function (data) {
+                //console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
+                var blob;
+                for (var i = 0; i < blobs.length; i++) {
+                    if (socket.id == blobs[i].id) {
+                        blob = blobs[i];
+                    }
+                }
+                blob.x = data.x;
+                blob.y = data.y;
+                blob.r = data.r;
+            }
+        );
+
+
+
+        socket.on('disconnect', function () {
+            console.log("Client has disconnected");
+        });
+    }
+);
