699dd9b3d0bedb702d212ad1f38b2508fc561f88
KChen <karencyuan@gmail.com>
2019-02-28 01:15:27 -0800

    server update

--- server.js
+++ server.js
@@ -11,3 +11,46 @@ function listen() {
 }
 
 app.use(express.static('public/client/'));
+
+
+// WebSocket Portion
+// WebSockets work with the HTTP server
+var io = require('socket.io')(server);
+
+setInterval(heartbeat, 33);
+
+function heartbeat() {
+    // io.sockets.emit('heartbeat', blobs);
+}
+
+
+
+// Register a callback function to run when we have an individual connection
+// This is run for each individual user that connects
+io.sockets.on('connection',
+    // We are given a websocket object in our function
+    function (socket) {
+
+        console.log("We have a new client: " + socket.id);
+
+
+        socket.on('start',
+            function (data) {
+                console.log("start");
+            }
+        );
+
+        socket.on('update',
+            function (data) {
+                //console.log(socket.id + " " + data.x + " " + data.y + " " + data.r);
+                console.log("update");
+            }
+        );
+
+
+
+        socket.on('disconnect', function () {
+            console.log("Client has disconnected");
+        });
+    }
+);
