e3f5c4f86e57d2e3c7e429275ac2fe9ecbc08328
KChen <karencyuan@gmail.com>
2019-02-27 19:37:39 -0800

    heroku setup

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-var express = require('express');
 
-var app = express();
-var server = app.listen(3000);
-var io = require('socket.io')(server);
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
 
+express()
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'ejs')
+    .get('/', (req, res) => res.render('pages/index'))
+    .listen(PORT, () => console.log(`Listening on ${PORT}`))
 app.use(express.static('public/client'))
+var io = require('socket.io')(server);
