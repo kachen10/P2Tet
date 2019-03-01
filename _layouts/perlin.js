69f600e24b4438be760863f58fc886bb58b9de7a
KChen <karencyuan@gmail.com>
2019-02-28 17:02:33 -0800

    drawing 2d perlin

--- _layouts/perlin.js
+++ _layouts/perlin.js
@@ -1,18 +1,40 @@
 
-var xoff = 0;
+var scale = 20;
+
+var start = 0;
+var increment = 0.02;
+var cols, rowsl
 
 
 function setup() {
-    createCanvas( 400, 400 )    
+    createCanvas( 400, 400 );
+    cols = floor(width /scale);
+    cols = floor(height /scale);
 }
 
 
 function draw() {
-    background(51);
+    
+    var yoff = 0;
 
-    var x = map(noise(xoff), 0, 1, 0, width);
-    var y = map(noise(xoff), 0, 1, 0, height);
-    xoff += 0.02;
+    loadPixels();
+    for (var x = 0; x < width; x++) {
+        var xoff = 0;
+        for (var y = 0; y < height; y++) {
 
-    ellipse(x, 200, 24, 24);
+            var index = ( x + y * width) * 4;
+            var r = noise(xoff, yoff) * 255;
+            pixels[index+0] = r;
+            pixels[index+1] = r;
+            pixels[index+2] = r;
+            pixels[index+3] = r;
+
+            xoff += increment;
+        }  
+        yoff += increment;
+    }
+    updatePixels();
+    // noLoop();
+    
+    
 }
